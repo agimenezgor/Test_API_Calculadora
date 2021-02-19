53412c912af1de9a1d1b6a71a5cf9e2b86fc129a
agimenezgor <alvarogimenez88@gmail.com>
2021-02-19 12:26:30 +0100

    green: user, login: pasamos datos correctos

--- test/user/user.test.js
+++ test/user/user.test.js
@@ -86,7 +86,7 @@ describe('User tests', () => {
             // Act
             const response = await loginFetch(user);
             // Assert
-            expect(response.message).toBe("La contraseña es incorrecta")
+            expect(response.message).toBe("Sesión iniciada correctamente")
         })
     })
 
