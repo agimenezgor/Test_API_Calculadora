28efecfb53954f09eeccb29c57d584db1c3803e9
agimenezgor <alvarogimenez88@gmail.com>
2021-02-20 19:53:40 +0100

    order: todos los datos a crear implementados

--- test/order/ordertest.js
+++ test/order/ordertest.js
@@ -1,16 +1,70 @@
-// 1 - creamos un nuevo usuario
-// 2 - creamos las referencias de muestra
-// 3 - comprobamos que se han guardado mediante la función getAll
-// 4 - creamos el objeto en el que meteremos la cantidad de palets que hay de cada referencia
-
 // importamos servicios de usuario
+const deleteDefaultUser = require('../../services/deleteDefaultUser.js/deleteDefaultUser');
+const initDefaultUser = require('../../services/initDefaultUser/initDefaultUser');
 
 // importamos servicios de proveedor
+const supplierRegisterFetch = require("../supplier/fetchData/registerFetch");
+const supplierDeleteFetch = require("../supplier/fetchData/deleteFetch");
 
 // importamos servicios de referencias
+const registerFetch = require("./fetchData/registerFetch");
+const deleteFetch = require("./fetchData/deleteFetch");
+const getAllFetch = require("./fetchData/getAllFetch");
 
 // importamos servicios de pedidos
+const orderFetch = require("./fetchData/orderFetch");
 
+// Usuario por defecto
+let user = Object();
+user.name = "Order user test";
+user.email = "orderUser@test.com";
+user.password = "12345678";
+
+// Proveedores por defecto
+// Proveedor por palets
+const paletsSupplier = Object();
+            paletsSupplier.name = "Test reference supplier";
+            paletsSupplier.number = 12345677;
+            paletsSupplier.days = 3;
+            paletsSupplier.calculateType = "Palets";
+            paletsSupplier.minPalets = 26;
+            paletsSupplier.maxPalets = 26;
+// Proveedor por kilos
+const kilosSupplier = Object();
+            kilosSupplier.name = "Test reference supplier";
+            kilosSupplier.number = 12345678;
+            kilosSupplier.days = 3;
+            kilosSupplier.calculateType = "Kilos";
+            kilosSupplier.minPalets = 26;
+            kilosSupplier.maxPalets = 26;
+// Proveedor por Franco
+const moneySupplier = Object();
+            moneySupplier.name = "Test reference supplier";
+            moneySupplier.number = 12345679;
+            moneySupplier.days = 3;
+            moneySupplier.calculateType = "Franco";
+            moneySupplier.minPalets = 26;
+            moneySupplier.maxPalets = 26;
+
+// Referencias por defecto
+const defaultReference = Object();
+            defaultReference.name = "Test reference";
+            defaultReference.number = 12345677;
+            defaultReference.conditioning = 48;
+            defaultReference.facing = 2;
+            defaultReference.sales = 100;
+const defaultReference2 = Object();
+            defaultReference2.name = "Test reference 2";
+            defaultReference2.number = 12345678;
+            defaultReference2.conditioning = 48;
+            defaultReference2.facing = 2;
+            defaultReference2.sales = 100;
+const defaultReference3 = Object();
+            defaultReference3.name = "Test reference 3";
+            defaultReference3.number = 12345679;
+            defaultReference3.conditioning = 48;
+            defaultReference3.facing = 2;
+            defaultReference3.sales = 100;
     
 beforeAll( async () => {
     // iniciamos el usuario
